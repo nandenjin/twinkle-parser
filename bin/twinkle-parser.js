5f5b3a5f4c225fcee2e21b5d2e6221a81e7ff147
Kazumi Inada <hello@nandenjin.com>
2019-11-15 17:57:09 +0900

    Add help option

--- bin/twinkle-parser.js
+++ bin/twinkle-parser.js
@@ -1,13 +1,29 @@
 #!/usr/bin/env node
 const fs = require('fs')
+const parseArgs = require('minimist')
 const Iconv = require('iconv').Iconv
 const parse = require('../dist/index.js').default
 
 const iconv = new Iconv('Shift_JIS', 'UTF-8//TRANSLIT//IGNORE')
+const argv = parseArgs(process.argv.slice(2))
 
-const filename = process.argv[ 2 ]
+// * Help
+if (argv.h || argv.help) {
+  console.log(
+    `* twinkle-parser
+Usage: twinkle-parser PATH_TO_SOURCE_CSV
+
+Options:
+-h / --help : See this help
+`
+  )
+  process.exit(0)
+}
+
+const filename = argv._[0]
 if (!filename) {
   console.error('ERROR: Filename is required')
+  console.error('Add -h / --help option to see usage')
   process.exit(1)
 }
 
