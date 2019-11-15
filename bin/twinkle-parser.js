cfa16f36e298312c797ad7d3ba2db45eabb6e7f5
Kazumi Inada <hello@nandenjin.com>
2019-11-15 18:03:19 +0900

    Add output/prettify options (#31)

--- bin/twinkle-parser.js
+++ bin/twinkle-parser.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 const fs = require('fs')
+const path = require('path')
 const parseArgs = require('minimist')
 const Iconv = require('iconv').Iconv
 const parse = require('../dist/index.js').default
@@ -14,13 +15,23 @@ if (argv.h || argv.help) {
 Usage: twinkle-parser PATH_TO_SOURCE_CSV
 
 Options:
+-o FILENAME / --out FILENAME : Export JSON to specified file
+-p / --pretty : Prettify output
 -h / --help : See this help
 `
   )
   process.exit(0)
 }
 
+// Input filename
 const filename = argv._[0]
+
+// Prettify flag
+const prettyFlag = argv.p || argv.pretty
+
+// Output filepath
+const outPath = argv.o || argv.out
+
 if (!filename) {
   console.error('ERROR: Filename is required')
   console.error('Add -h / --help option to see usage')
@@ -30,5 +41,10 @@ if (!filename) {
 const csvData = iconv.convert(fs.readFileSync(filename)).toString()
 
 const result = parse(csvData)
+const outputJSON = JSON.stringify(result, null, prettyFlag ? 2 : 0)
 
-process.stdout.write(JSON.stringify(result))
+if (outPath) {
+  fs.writeFileSync(path.resolve(outPath), outputJSON)
+} else {
+  process.stdout.write(outputJSON)
+}
