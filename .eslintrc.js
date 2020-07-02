f88ef3070d96dcbdfdd874e26f63578f1d9a0109
Kazumi Inada <hello@nandenjin.com>
2020-07-02 15:39:30 +0900

    [Chore] Automated lintfix

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,30 +2,30 @@ module.exports = {
   root: true,
   plugins: ['@typescript-eslint'],
   parserOptions: {
-    parser: '@typescript-eslint/parser'
+    parser: '@typescript-eslint/parser',
   },
   env: {
-    'node': true,
-    'jest/globals': true
+    node: true,
+    'jest/globals': true,
   },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
-    'prettier/@typescript-eslint'
+    'prettier/@typescript-eslint',
   ],
-  plugins: [
-    'jest',
-    'prettier'
-  ],
+  plugins: ['jest', 'prettier'],
   rules: {
     'no-console': 'off',
     '@typescript-eslint/no-unused-vars': 'error',
     '@typescript-eslint/explicit-function-return-type': 'off',
-    'prettier/prettier': ['error', {
-      semi: false,
-      singleQuote: true,
-      arrowParens: 'avoid'
-    }]
-  }
+    'prettier/prettier': [
+      'error',
+      {
+        semi: false,
+        singleQuote: true,
+        arrowParens: 'avoid',
+      },
+    ],
+  },
 }
