cc77698c29362d6a1586fcafd2d8ba24e0aea23b
Kazumi Inada <hello@nandenjin.com>
2021-01-03 14:37:58 +0900

    refactor: Migrate from iconv to iconv-lite

--- bin/twinkle-parser.js
+++ bin/twinkle-parser.js
@@ -4,11 +4,25 @@
 const fs = require('fs')
 const path = require('path')
 const parseArgs = require('minimist')
-const Iconv = require('iconv').Iconv
+const iconv = require('iconv-lite')
 const parse = require('../dist/index.js').default
 
-const iconv = new Iconv('Shift_JIS', 'UTF-8//TRANSLIT//IGNORE')
 const argv = parseArgs(process.argv.slice(2))
+
+const fieldKeys = [
+  'title',
+  'termStr',
+  'terms',
+
+  'periodStr',
+  'periods',
+
+  'rooms',
+  'instructors',
+
+  'overview',
+  'remarks',
+]
 
 // * Help
 if (argv.h || argv.help) {
@@ -19,6 +33,7 @@ Usage: twinkle-parser PATH_TO_SOURCE_CSV
 Options:
 -o FILENAME / --out FILENAME : Export JSON to specified file
 -p / --pretty : Prettify output
+--fields / : Field keys to output
 -h / --help : See this help
 `
   )
@@ -31,6 +46,9 @@ const filename = argv._[0]
 // Prettify flag
 const prettyFlag = argv.p || argv.pretty
 
+// Fields
+const fields = argv.fields ? argv.filelds.split(',') : fieldKeys
+
 // Output filepath
 const outPath = argv.o || argv.out
 
@@ -40,9 +58,20 @@ if (!filename) {
   process.exit(1)
 }
 
-const csvData = iconv.convert(fs.readFileSync(filename)).toString()
+const csvData = iconv.decode(fs.readFileSync(filename), 'Shift_JIS')
 
-const result = parse(csvData)
+const parsed = parse(csvData, { fields })
+const result = {}
+
+for (const id in parsed) {
+  result[id] = {}
+  for (const key in parsed[id]) {
+    if (fields.includes(key)) {
+      result[id][key] = parsed[id][key]
+    }
+  }
+}
+
 const outputJSON = JSON.stringify(result, null, prettyFlag ? 2 : 0)
 
 if (outPath) {
