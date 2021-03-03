73d1a73eec7fe58bbf0ed0759a81057d9f44ab9c
Kazumi Inada <hello@nandenjin.com>
2021-03-03 15:58:03 +0900

    chore: Remove eslint-plugin-prettier and unnecessary config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,29 @@
 module.exports = {
-  root: true,
-  plugins: ['@typescript-eslint', 'jest', 'prettier'],
-  parserOptions: {
-    parser: '@typescript-eslint/parser',
-  },
-  env: {
-    node: true,
-    'jest/globals': true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'prettier/@typescript-eslint',
-  ],
-  rules: {
-    'no-console': 'off',
-    '@typescript-eslint/no-unused-vars': 'error',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    'prettier/prettier': [
-      'error',
-      {
-        semi: false,
-        singleQuote: true,
-        arrowParens: 'avoid',
-      },
+    root: true,
+    plugins: ['@typescript-eslint', 'jest'],
+    parserOptions: {
+        parser: '@typescript-eslint/parser',
+    },
+    env: {
+        node: true,
+        'jest/globals': true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
     ],
-  },
+    rules: {
+        'no-console': 'off',
+        '@typescript-eslint/no-unused-vars': 'error',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        'prettier/prettier': [
+            'error',
+            {
+                semi: false,
+                singleQuote: true,
+                arrowParens: 'avoid',
+            },
+        ],
+    },
 }
