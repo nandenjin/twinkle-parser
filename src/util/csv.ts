03b52e5979b526dab6dc8cee3a47ba583a140f71
Kazumi Inada <hello@nandenjin.com>
2021-04-02 20:44:01 +0900

    feat: Add repairCSV()

---
+++ src/util/csv.ts
@@ -0,0 +1,20 @@
+/**
+ * Repair csvString that is not correctly escaped and make to be able to be parsed
+ * - Escape double quotes that are not escaped
+ * @param csvString original csv data
+ * @returns csvString that is correctly escaped
+ */
+export const repairCSV = (csvString: string) =>
+  csvString.replace(/"([\s\S]*?)"(\s*(?:[,\n]\s*)|$)/gm, (...s) => {
+    const value = s[1]
+    let repairedValue = value.replace(
+      /"/g,
+      (_: unknown, __: unknown, i: number) => {
+        if (value.slice(i - 1).match(/^.?""/)) {
+          return '"'
+        }
+        return '""'
+      }
+    )
+    return `"${repairedValue}"` + s[2]
+  })
---
+++ tests/util/csv.test.ts
@@ -0,0 +1,29 @@
+import { repairCSV } from '../../src/util/csv'
+
+describe('repairCsv', () => {
+  it('does nothing for valid CSV', () => {
+    const validCSVStrings = [
+      '',
+      'a,b,c',
+      'a,b,c\nd,e,f',
+      '"a","b","c"\n"d","e","f"',
+      '"a", "b", "c"\n',
+      '"abc", "d""e""f"',
+      '"a""", "b",c,\n"d", """"'
+    ]
+    for (const s of validCSVStrings) {
+      expect(repairCSV(s)).toBe(s)
+    }
+  })
+
+  it('repairs non-escaped double quotations', () => {
+    const csvStrings = [
+      ['"""', '""""'],
+      ['"a"b"c", "def"', '"a""b""c", "def"'],
+      ['"abc", "d"e"\n"a"b"c"', '"abc", "d""e"\n"a""b""c"']
+    ]
+    for (const s of csvStrings) {
+      expect(repairCSV(s[0])).toBe(s[1])
+    }
+  })
+})
