1c8b7074a69e19ffb93f9f9e701f4812a5abc3be
Kazumi Inada <hello@nandenjin.com>
2023-07-21 17:28:40 +0900

    chore: Remove deprecated syntax from jest.config

--- jest.config.ts
+++ jest.config.ts
@@ -3,12 +3,12 @@ import type { Config } from '@jest/types'
 const config: Config.InitialOptions = {
   moduleFileExtensions: ['ts', 'js'],
   transform: {
-    '^.+\\.ts$': 'ts-jest',
-  },
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json',
-    },
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json',
+      },
+    ],
   },
   testMatch: ['**/tests/**/*.test.ts'],
 }
