14dda268ac4f193414480d26569c6286ff026e69
Kazumi Inada <hello@nandenjin.com>
2023-07-21 18:01:29 +0900

    chore: Add tests for missed double quotations (#295)
    * chore: Add tests for missed double quotation
    
    * chore: Add test case

--- tests/util/csv.test.ts
+++ tests/util/csv.test.ts
@@ -3,6 +3,7 @@ import { repairCSV } from '../../src/util/csv'
 describe('repairCsv', () => {
   it('does nothing for valid CSV', () => {
     const validCSVStrings = [
+      '',
       '"a","b","c"\n"d","e","f"',
       '"a","b","c"\n',
       '"abc","d""e""f"',
@@ -19,6 +20,23 @@ describe('repairCsv', () => {
       ['"a"b"c","def"', '"a""b""c","def"'],
       ['"abc","d"e"\n"a"b"c"', '"abc","d""e"\n"a""b""c"'],
     ]
+    for (const s of csvStrings) {
+      expect(repairCSV(s[0])).toBe(s[1])
+    }
+  })
+
+  it('repairs non-matching double quotations', () => {
+    const csvStrings = [
+      // A cell contains non-escaped double quotation with a comma
+      ['"abc",def"', '"abc"",def"'],
+
+      // A cell with missing all of its double quotation
+      ['"abc",def', '"abc","def"'],
+
+      // The first cell with missing its first double quotation
+      ['abc"', '"abc"'],
+    ]
+
     for (const s of csvStrings) {
       expect(repairCSV(s[0])).toBe(s[1])
     }
